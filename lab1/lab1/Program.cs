a86395725689e12dc576e341f730f7d190becd53
ksenia <ksenia.stels@gmail.com>
2017-06-03 20:48:21 +0300

    value entry

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -10,10 +10,29 @@ namespace lab1
     {
         static void Main(string[] args)
         {
+            Console.Write("N=");
             string s = Console.ReadLine();
             int size = int.Parse(s);
+            long el;
             Console.WriteLine(size);
 
+            long[,] a = new long[size,size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("el" + i + j + "= ");
+                    el = Convert.ToInt32(Console.ReadLine());
+                    a[i,j] = el;
+                }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < size; j++)
+                    Console.Write(a[i, j] + "   ");
+            }
+            
+
+            Console.ReadKey();
         }
     }
 }
