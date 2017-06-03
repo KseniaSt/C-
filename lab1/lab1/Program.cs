5f41d49bf5d11cedfc2a740a140c747eb400d74d
ksenia <ksenia.stels@gmail.com>
2017-06-03 21:10:27 +0300

    Dijkstra’s algorithm

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -13,10 +13,19 @@ namespace lab1
             Console.Write("N=");
             string s = Console.ReadLine();
             int size = int.Parse(s);
-            long el;
+            int el, minindex, min, temp;
             Console.WriteLine(size);
+            
+            int[,] a = new int[size,size];
+            int[] d = new int[size];
+            int[] v = new int[size];
 
-            long[,] a = new long[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                d[i] = 10000;
+                v[i] = 1;
+            }
+
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
                 {
@@ -24,15 +33,44 @@ namespace lab1
                     el = Convert.ToInt32(Console.ReadLine());
                     a[i,j] = el;
                 }
+
             for (int i = 0; i < size; i++)
             {
                 Console.WriteLine("\n");
                 for (int j = 0; j < size; j++)
                     Console.Write(a[i, j] + "   ");
             }
-            
 
+            d[0] = 0;
+            do {
+		        minindex = 10000;
+		        min = 10000;
+		for (int i = 0; i<size; i++)
+		{ // Если вершину ещё не обошли и вес меньше min
+			if ((v[i] == 1) && (d[i]<min))
+			{ 
+				min = d[i];
+				minindex = i;
+			}
+		}
+        if (minindex != 10000)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (a[minindex,i] > 0)
+                {
+                    temp = min + a[minindex,i];
+                    if (temp < d[i])
+                        d[i] = temp;
+                }
+            }
+            v[minindex] = 0;
+        }
+            } while (minindex < 10000);
+            for (int i = 0; i < size; i++)
+                Console.Write(d[i]);
             Console.ReadKey();
         }
     }
+    
 }
