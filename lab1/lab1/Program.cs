2c6ae6e297c500f123bdcea03e2749e6c9b776a0
ksenia <ksenia.stels@gmail.com>
2017-06-03 20:08:55 +0300

    size

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int size = int.Parse(s);
+            Console.WriteLine(size);
+
+        }
+    }
+}
