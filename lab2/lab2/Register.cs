f78b4466771a40aba0c7ebafa293654c0a0185db
ksenia <ksenia.stels@gmail.com>
2017-06-06 17:23:29 +0300

    create Register

---
+++ lab2/lab2/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Register
+    {
+        private string name;
+        private List<Trigger> reg;
+
+        public Register(string name)
+        {
+            this.name = name;
+            reg = new List<Trigger>();
+        }
+
+       
+    }
+}
