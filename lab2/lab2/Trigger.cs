51dbfd9a86dd89f2433b7923e8b059421438e24b
ksenia <ksenia.stels@gmail.com>
2017-06-06 16:03:59 +0300

    abstract class Trigger

---
+++ lab2/lab2/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    abstract class Trigger
+    {
+        protected string name;
+        protected bool input1;
+        protected bool input2;
+        protected bool output;
+
+        public string GetName
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
